ed2945775fe8e8ed3f24c6688c200cbf96c2aba8
costintudi <costinrch46@gmail.com>
2019-12-24 15:00:06 +0200

    Git repository restructured

--- Demo/DEMO/src/Person.java
+++
@@ -1,6 +0,0 @@
-public abstract class Person {
-    public abstract void isSalutedBy(Person person);
-    public abstract void salute(Doctor doctor);
-    public abstract void salute(Engineer engineer);
-    public abstract void salute(Lawyer lawyer);
-}
